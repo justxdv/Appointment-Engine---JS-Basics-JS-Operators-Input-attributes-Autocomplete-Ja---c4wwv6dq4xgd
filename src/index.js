480e7be8c522e48635e2024dd73f5d5681252c6e
Digvijay Sethi <59555227+justxdv@users.noreply.github.com>
2023-02-24 13:52:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,52 +6,89 @@ const port = 8080
 // Parse JSON bodies (as sent by API clients)
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const con = require('./connector')
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+const  con  = require('./connector');
+//const refreshAll = require('./createdatabase');
 
-// GET all events
-app.get('/api/events', (req, res) => {
-  con.query('SELECT * FROM events', (err, result) => {
-    if (err) return res.status(500).json({ error: err.message })
-    res.status(200).json({ result })
-  })
+app.get('/api/events',(req,res)=>{
+    con.query('select * from events',(err,result)=>{
+        res.status(200);
+        res.send(result)
+        //console.log(result)
+    })
 })
 
-// GET events by date
-app.get('/api/events/:date', (req, res) => {
-  const date = req.params.date
-  con.query(`SELECT * FROM events WHERE date = '${date}'`, (err, result) => {
-    if (err) return res.status(500).json({ error: err.message })
-    res.status(200).json({ result })
-  })
+app.get('/api/events/:date',(req,res)=>{
+    const d=req.params.date;
+    const sql=`select * from events where date="${d}"`
+    con.query(sql,(err,result)=>{
+        res.status(200);
+        res.send(result)
+        //console.log(result)
+    })
 })
 
-// POST a new or update an existing event
-app.post('/api/events/:date', (req, res) => {
-  const date = req.params.date
-  const slots = JSON.stringify(req.body.slots)
-  con.query(`INSERT INTO events (date, slots) VALUES ('${date}', '${slots}') ON DUPLICATE KEY UPDATE slots='${slots}'`, (err, result) => {
-    if (err) return res.status(500).json({ error: err.message })
-    res.status(200).json({ msg: "Slot Added" })
-  })
+app.post('/api/events/:date',(req,res)=>{
+    const d=req.params.date;
+    const body=req.body.slots;
+    let flag=0;
+    let sql;
+    con.query(`select id from events where date="${d}"`,(err,result)=>{
+        if(err)res.send(err)
+        else if(result.length>0){
+            console.log('updating');
+            sql=`update events set slot1=${body[0]}, slot2=${body[1]}, slot3=${body[2]}, slot4=${body[3]}, slot5=${body[4]}, slot6=${body[5]}, slot7=${body[6]}, slot8=${body[7]}, slot9=${body[8]}, slot10=${body[9]}, slot11=${body[10]}, slot12=${body[11]}, slot13=${body[12]}, slot14=${body[13]} where id=${result[0].id} `
+            con.query(sql,(err,result)=>{
+                if(err) res.send(err);
+                res.status(200).json({ msg: "Slot Added"});
+                //console.log((sql))
+            })
+            flag=1;
+        }
+        else{
+            
+                console.log('adding');
+                sql=`insert into events(date,slot1,slot2,slot3,slot4,slot5,slot6,slot7,slot8,slot9,slot10,slot11,slot12,slot13,slot14) values ("${d}",${body[0]},${body[1]},${body[2]},${body[3]},${body[4]},${body[5]},${body[6]},${body[7]},${body[8]},${body[9]},${body[10]},${body[11]},${body[12]},${body[13]}) `
+                con.query(sql,(err,result)=>{
+                    if(err) res.send(err);
+                    res.status(200).json( {msg: "Slot Added"});
+                   // console.log((sql))
+                })
+        }
+    })
+    
+    
+    
+
+    // const sql=`update events set slot1=${body[0]} slot2=${body[1]} slot3=${body[2]} slot4=${body[3]} slot5=${body[4]} slot6=${body[5]} slot7=${body[6]} slot8=${body[7]} slot9=${body[8]} slot10=${body[9]} slot11=${body[10]} slot12=${body[11]} slot13=${body[12]} slot14=${body[13]} where date="${d}" `
+    
+    // console.log(sql)
 })
 
-// GET all users
-app.get('/api/names', (req, res) => {
-  con.query('SELECT * FROM users', (err, result) => {
-    if (err) return res.status(500).json({ error: err.message })
-    res.status(200).json({ result })
-  })
+app.get('/api/names',(req,res)=>{
+    con.query('select * from user',(err,result)=>{
+        res.status(200)
+        res.send(result)
+        //console.log(result)
+    })
 })
 
-// POST a new user
-app.post('/api/names', (req, res) => {
-  const name = req.body.name
-  con.query(`INSERT INTO users (name) VALUES ('${name}')`, (err, result) => {
-    if (err) return res.status(500).json({ error: err.message })
-    res.status(200).json({ msg: "Name Added" })
-  })
+app.post('/api/names',(req,res)=>{
+    
+    const body=req.body.name;
+    let flag=0;
+   
+    const sql=`insert into user(name) values ("${body}") `
+    con.query(sql,(err,result)=>{
+        
+        res.status(200).json({msg: "Name Added"});
+        //console.log((sql))
+    })
+    // console.log(sql)
 })
+
+app.listen(port, () =>{ 
+    
+    console.log(`App listening on port ${port}!`)})
 
 module.exports = app;
