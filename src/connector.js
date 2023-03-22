816c0b311cd9417ee2c92087d2dec3eb9ffeed52
Digvijay Sethi <59555227+justxdv@users.noreply.github.com>
2023-03-22 17:53:02 +0530

    Update connector.js

--- src/connector.js
+++ src/connector.js
@@ -1,6 +1,5 @@
 var mysql = require('mysql');
 
-
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -8,7 +7,6 @@ var con = mysql.createConnection({
     database: "test",
     multipleStatements: true
 });
-
 
 con.connect(function (err) {
     if (err) return console.log("failed to connect to mysql server/ database", err);
